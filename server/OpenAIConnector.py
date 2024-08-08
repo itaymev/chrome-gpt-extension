06395a3dbffa67dbafd794cad80fe13a79f0e2eb
Mevorach, Itay <itay.mevorach@intel.com>
2024-08-08 11:00:27 -0700

    error and debug msgs

--- server/OpenAIConnector.py
+++ server/OpenAIConnector.py
@@ -7,11 +7,12 @@ class OpenAIConnector:
     def __init__(self, deployment_name = None) -> None:
         if deployment_name is None:
             deployment_name = DEFAULT_DEPLOYMENT_NAME
-            # print("INFO - %s: using default deployment: %s"%(__file__, deployment_name,))
+        
         self.deployment_name = deployment_name
+        # print("INFO - %s: using deployment: %s"%(__file__, deployment_name,))
 
         self.client = AzureOpenAI(
-                        api_version = "2023-07-01-preview",
+                        api_version = "2024-02-01", # This is the latest version as of 8/8/2024
                         api_key = self.get_ai_key(),
                         base_url = self.get_ai_endpoint(),
                         default_headers={"Ocp-Apim-Subscription-Key" :self.get_ai_key()},
@@ -30,9 +31,9 @@ class OpenAIConnector:
             max_tokens = 4096
         )
 
-        # print(f"Prompt tokens: {completion.usage.prompt_tokens}")
-        # print(f"Completion tokens: {completion.usage.completion_tokens}")
-        # print(f"GPT finish reason: {completion.choices[0].finish_reason}")
+        # print(f"INFO - {__file__}: Prompt tokens: {completion.usage.prompt_tokens}")
+        # print(f"INFO - {__file__}: Completion tokens: {completion.usage.completion_tokens}")
+        # print(f"INFO - {__file__}: GPT finish reason: {completion.choices[0].finish_reason}")
     
         gpt_response = completion.choices[0].message.content
 
@@ -40,16 +41,20 @@ class OpenAIConnector:
 
     def get_ai_key(self) -> str:
         if "OPENAI_KEY" in os.environ:
+            # print("INFO - %s: Found OPENAI_KEY environment variable."%(__file__,))
             return os.environ["OPENAI_KEY"]
         elif "openai_key" in os.environ:
+            # print("INFO - %s: Found OPENAI_KEY environment variable."%(__file__,))
             return os.environ["openai_key"]
         else:
-            raise Exception("OpenAI key not found in environment variables")
+            raise Exception("ERROR - %s: OpenAI key not found in environment variables"%(__file__,))
         
     def get_ai_endpoint(self) -> str:
         if "OPENAI_ENDPOINT" in os.environ:
+            # print("INFO - %s: Found OPENAI_ENDPOINT environment variable."%(__file__,))
             return os.environ["OPENAI_ENDPOINT"]
         elif "openai_endpoint" in os.environ:
+            # print("INFO - %s: Found OPENAI_ENDPOINT environment variable."%(__file__,))
             return os.environ["openai_endpoint"]
         else:
-            raise Exception("OpenAI endpoint not found in environment variables")
+            raise Exception("ERROR - %s: OpenAI endpoint not found in environment variables"%(__file__,))
